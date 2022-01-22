107e1b70abf9f6ff679c487dea1e3567a3a2ab31
Chanukavidanapathirana <97946093+Chanukavidanapathirana@users.noreply.github.com>
2022-01-22 13:53:24 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean isOnRepeat = true;
+        
+        
+        while(isOneRepeat){
+        System.out.println("Playing same song again");
+        System.out.println("Would you like to take this song off of repeat?If so, answer yes");
+        String userInput=input.next();
+            if(userInput.equal("yes")){
+                isOneRepeat=false;
+            }
+        }
+        System.out.println("playing next song")
     }
 }
