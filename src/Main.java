f6bc2617d9bb438055b859c34038d72d7f86c881
Chanukavidanapathirana <97946093+Chanukavidanapathirana@users.noreply.github.com>
2022-03-12 17:19:57 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+import java.util.*;
 public class Main {
+    public static void DeveloperTeaTime(){
+    System.out.println("Developer team time");
+    System.out.println("Enter some thing :");
+    Scanner input= new Scanner(System.in);
+    input.next();
+     System.out.println("Go and eat something");
+      
+        
+    }
 
     public static void main(String[] args) {
 
